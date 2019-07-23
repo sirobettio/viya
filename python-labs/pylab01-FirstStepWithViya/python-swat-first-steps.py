8530911ccf6dfb2c79da6957dbde608b8db2e883
sirobettio <siro.bettio@gmail.com>
2019-07-23 14:59:27 +0200

    python-swat-first-steps

--- python-labs/pylab01-FirstStepWithViya/python-swat-first-steps.py
+++ python-labs/pylab01-FirstStepWithViya/python-swat-first-steps.py
@@ -7,12 +7,30 @@ Created on Sun Dec  2 16:13:06 2018
 import swat
 import os
 
-os.environ['CAS_CLIENT_SSL_CA_LIST']=r"C:/dev/viyacertificates/lab01-viya34smp/vault-ca.crt"
-#os.environ['REQUESTS_CA_BUNDLE']=r"C:/dev/viyacertificates/lab01-viya34smp/vault-ca.crt"
+# lab10 environment:
+# cas_shared_default_srv --> lab10-viyac1.lab.local     
+#                            CAS Server configured with it's own certificate signed by Nordic Labs
+#                            This certificate is in the windows trust store
+cas_shared_default_srv = "lab10-viyac1.lab.local"
 
-conn = swat.CAS('viya34smp.nordiclab.sashq-r.openstack.sas.com', 5570)
-csvtbl = conn.read_csv('https://raw.githubusercontent.com/sassoftware/sas-viya-programming/master/data/cars.csv')
-h = csvtbl.head()
-print(h)
+# cas_shared_dev_srv     --> lab10-viyadev.lab.local    
+#                            CS Server used the default Viya certificate
+cas_shared_dev_srv     = "lab10-viyadev.lab.local"
+
+#---------------- SELECT THE CAS SERVER ----------------------
+selected_cas = cas_shared_default_srv
+#-------------------------------------------------------------
+
+if (selected_cas == cas_shared_dev_srv):
+    os.environ['CAS_CLIENT_SSL_CA_LIST']=r"/opt/sasviyarepo/lab10/viya-certificates/vault-ca.crt"
+
+# Connect to the cas server and get a CAS Session
+conn = swat.CAS(selected_cas, 5570)
+print(conn)
+
+# Get server status
+print(conn.builtins.serverStatus())
+
+# End CAS Session
 r=conn.session.endSession()
 print(r)
